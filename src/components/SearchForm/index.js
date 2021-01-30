2c067a389febeeb95014aa3bb06e2e01d59bd101
Ben Galloway <bdgalloway85@gmail.com>
2021-01-29 18:02:45 -0600

    change format

--- src/components/SearchForm/index.js
+++ src/components/SearchForm/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { TextField } from '@material-ui/core';
 
+
 const useStyles = makeStyles((theme) => ({
   root: {
     '& > *': {
